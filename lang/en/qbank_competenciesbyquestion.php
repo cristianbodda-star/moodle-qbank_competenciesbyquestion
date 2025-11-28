73b878e14fa72368102413bb3760409c4f245d13
cristianbodda-star <cristianbodda@gmail.com>
2025-11-28 11:19:35 +0100

    qbank_competenciesbyquestion.php

--- lang/en/qbank_competenciesbyquestion.php
+++ lang/en/qbank_competenciesbyquestion.php
@@ -1,6 +1,14 @@
 <?php
-// Language strings for plugin qbank_competenciesbyquestion.
+// Language strings for qbank_competenciesbyquestion.
 
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Competencies by question';
+
+$string['columncompetencies'] = 'Competency';
+$string['editcompetency'] = 'Assign competency';
+$string['competency'] = 'Competency';
+$string['competency_none'] = 'No competency';
+
+$string['editcompetencypagetitle'] = 'Assign competency to question';
+$string['editcompetencysaved'] = 'Competency mapping saved.';
