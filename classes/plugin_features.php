4d2b35a5ea8c3d9b24877b59039df2eafbebe63c
cristianbodda-star <cristianbodda@gmail.com>
2025-11-28 11:20:14 +0100

    Update plugin_features.php

--- classes/plugin_features.php
+++ classes/plugin_features.php
@@ -1,39 +1,30 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-namespace qbank_competenciesbyquestion;
+// Entry point for the qbank_competenciesbyquestion plugin.
 
 defined('MOODLE_INTERNAL') || die();
 
+namespace qbank_competenciesbyquestion;
+
 use core_question\local\bank\plugin_features_base;
 use core_question\local\bank\view;
+use qbank_competenciesbyquestion\columns\competency_column;
 
 /**
- * Plugin features for the qbank_competenciesbyquestion plugin.
+ * Plugin feature class for the question bank plugin.
+ *
+ * Registers extra columns in the question bank view.
  */
-class plugin_features extends plugin_features_base {
+class plugin_feature extends plugin_features_base {
 
     /**
-     * Colonne aggiuntive nella question bank.
-     * Per ora nessuna; le aggiungeremo in seguito.
+     * Extra columns provided by this plugin.
      *
      * @param view $qbank
      * @return array
      */
     public function get_question_columns(view $qbank): array {
-        return [];
+        return [
+            new competency_column($qbank),
+        ];
     }
 }
