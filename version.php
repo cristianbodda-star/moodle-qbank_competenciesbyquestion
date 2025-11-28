7373f778a823b8ae6213bd29248d2a749d372374
cristianbodda-star <cristianbodda@gmail.com>
2025-11-28 11:17:58 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,23 +1,15 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - http://moodle.org/.
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'qbank_competenciesbyquestion';
-$plugin->version   = 2025112700;
-$plugin->requires  = 2022041900; // Minimo Moodle 4.0.
+$plugin->component = 'qbank_competenciesbyquestion'; // Full name of the plugin.
+$plugin->version   = 2025112800;                     // YYYYMMDDHH (data versione).
+$plugin->requires  = 2022041900;                     // Minimo Moodle 4.0.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
